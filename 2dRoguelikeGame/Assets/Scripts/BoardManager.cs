8e7d5bb33601f291f3ada77c8f1f3c730f05cf4f
RuoFeng207 <99081157@mydavinci.nl>
2026-03-01 15:40:00 +0100

    3D array

--- 2dRoguelikeGame/Assets/Scripts/BoardManager.cs
+++ 2dRoguelikeGame/Assets/Scripts/BoardManager.cs
@@ -5,34 +5,45 @@ public class BoardManager : MonoBehaviour
 {
    private Tilemap m_Tilemap;
 
-   public int Width;
-   public int Height;
-   public Tile[] GroundTiles;
-   public Tile[] WallTiles; 
+    public int Width;
+    public int Height;
+    public Tile[] GroundTiles;
+    public Tile[] WallTiles;
+
+    public class CellData
+    {
+        public bool Passable;
+    }
+
+   private CellData[,] m_BoardData;
 
    // Start is called before the first frame update
    void Start()
-   {
-       m_Tilemap = GetComponentInChildren<Tilemap>();
+    {
+        m_Tilemap = GetComponentInChildren<Tilemap>();
 
-       for (int y = 0; y < Height; ++y)
-       {
-           for(int x = 0; x < Width; ++x)
-           {
+        m_BoardData = new CellData[Width, Height];
+
+        for (int y = 0; y < Height; ++y)
+        {
+            for(int x = 0; x < Width; ++x)
+            {
                 Tile tile;
-              
-               if(x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
-               {
-                   tile = WallTiles[Random.Range(0, WallTiles.Length)];
-               }
-               else
-               {
-                   tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
-               }
+                m_BoardData[x, y] = new CellData();
 
-               m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
-           }
-       }
-   }
+                if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
+                {
+                    tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    m_BoardData[x, y].Passable = false;
+                }
+                else
+                {
+                    tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                    m_BoardData[x, y].Passable = true;
+                }
 
+                m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+            }
+        }
+    }
 }
